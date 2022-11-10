395b577fb615244b14e50f2ff3fda5da2ebd55f2
Jameson Molnar <JMolnar@zscaler.com>
2022-11-10 10:36:38 -0500

    fix: tflint terraform_deprecated_index

--- modules/terraform-zscc-iam-aws/outputs.tf
+++ modules/terraform-zscc-iam-aws/outputs.tf
@@ -1,9 +1,9 @@
 output "iam_instance_profile_id" {
   description = "IAM Instance Profile Name"
-  value       = data.aws_iam_instance_profile.cc_host_profile_selected.*.name
+  value       = data.aws_iam_instance_profile.cc_host_profile_selected[*].name
 }
 
 output "iam_instance_profile_arn" {
   description = "IAM Instance Profile ARN"
-  value       = data.aws_iam_instance_profile.cc_host_profile_selected.*.arn
+  value       = data.aws_iam_instance_profile.cc_host_profile_selected[*].arn
 }
--- modules/terraform-zscc-network-aws/outputs.tf
+++ modules/terraform-zscc-network-aws/outputs.tf
@@ -5,30 +5,30 @@ output "vpc_id" {
 
 output "cc_subnet_ids" {
   description = "Cloud Connector Subnet ID"
-  value       = data.aws_subnet.cc_subnet_selected.*.id
+  value       = data.aws_subnet.cc_subnet_selected[*].id
 }
 
 output "workload_subnet_ids" {
   description = "Workloads Subnet ID"
-  value       = aws_subnet.workload_subnet.*.id
+  value       = aws_subnet.workload_subnet[*].id
 }
 
 output "public_subnet_ids" {
   description = "Public Subnet ID"
-  value       = aws_subnet.public_subnet.*.id
+  value       = aws_subnet.public_subnet[*].id
 }
 
 output "route53_subnet_ids" {
   description = "Route 53 Subnet ID"
-  value       = aws_subnet.route53_subnet.*.id
+  value       = aws_subnet.route53_subnet[*].id
 }
 
 output "nat_gateway_ips" {
   description = "NAT Gateway Public IP"
-  value       = data.aws_nat_gateway.ngw_selected.*.public_ip
+  value       = data.aws_nat_gateway.ngw_selected[*].public_ip
 }
 
 output "workload_route_table_ids" {
   description = "Workloads Route Table ID"
-  value       = aws_route_table_association.workload_rt_association.*.route_table_id
+  value       = aws_route_table_association.workload_rt_association[*].route_table_id
 }
