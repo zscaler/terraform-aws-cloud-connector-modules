1b8a51d3531e56ab0e6a0e507c1d459035ea5ce4
Jameson Molnar <JMolnar@zscaler.com>
2023-04-04 17:42:23 -0400

    fix: security group fixes
    - add lifecycle blocks
    - remove data source read dependency for non-byo resources

--- modules/terraform-zscc-sg-aws/outputs.tf
+++ modules/terraform-zscc-sg-aws/outputs.tf
@@ -1,19 +1,19 @@
 output "mgmt_security_group_id" {
   description = "Instance Management Security Group ID"
-  value       = data.aws_security_group.cc_mgmt_sg_selected[*].id
+  value       = var.byo_security_group ? data.aws_security_group.cc_mgmt_sg_selected[*].id : aws_security_group.cc_mgmt_sg[*].id
 }
 
 output "mgmt_security_group_arn" {
   description = "Instance Management Security Group ARN"
-  value       = data.aws_security_group.cc_mgmt_sg_selected[*].arn
+  value       = var.byo_security_group ? data.aws_security_group.cc_mgmt_sg_selected[*].arn : aws_security_group.cc_mgmt_sg[*].arn
 }
 
 output "service_security_group_id" {
   description = "Instance Service Security Group ID"
-  value       = data.aws_security_group.cc_service_sg_selected[*].id
+  value       = var.byo_security_group ? data.aws_security_group.cc_service_sg_selected[*].id : aws_security_group.cc_service_sg[*].id
 }
 
 output "service_security_group_arn" {
   description = "Instance Service Security Group ARN"
-  value       = data.aws_security_group.cc_service_sg_selected[*].arn
+  value       = var.byo_security_group ? data.aws_security_group.cc_service_sg_selected[*].arn : aws_security_group.cc_service_sg[*].arn
 }
