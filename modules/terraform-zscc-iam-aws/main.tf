44a3b5f4c4e7c11f8edf41ceb5d1a085b5005de3
Jameson Molnar <JMolnar@zscaler.com>
2023-07-24 13:21:42 -0400

    fix: remove callhome iam policy

--- modules/terraform-zscc-iam-aws/main.tf
+++ modules/terraform-zscc-iam-aws/main.tf
@@ -20,38 +20,6 @@ data "aws_iam_policy_document" "instance_assume_role_policy" {
 
 
 ################################################################################
-# Define AssumeRole access for CC callhome trust feature
-################################################################################
-data "aws_iam_policy_document" "cc_callhome_policy_document" {
-  version = "2012-10-17"
-  statement {
-    sid       = "AllowDelegationForCallhome"
-    effect    = "Allow"
-    actions   = ["sts:AssumeRole"]
-    resources = ["arn:aws:iam::223544365242:role/callhome-delegation-role"]
-  }
-}
-
-
-################################################################################
-# Create IAM Policy for CC callhome
-################################################################################
-resource "aws_iam_policy" "cc_callhome_policy" {
-  count       = var.byo_iam == false && var.cc_callhome_enabled ? var.iam_count : 0
-  description = "Policy which allows STS AssumeRole when attached to a user or role. Used for CC callhome"
-  name        = "${var.name_prefix}-cc-${count.index + 1}-callhome-policy-${var.resource_tag}"
-  policy      = data.aws_iam_policy_document.cc_callhome_policy_document.json
-}
-
-# Attach CC callhome policy to CC IAM Role
-resource "aws_iam_role_policy_attachment" "cc_callhome_policy_attachment" {
-  count      = var.byo_iam == false && var.cc_callhome_enabled ? var.iam_count : 0
-  policy_arn = aws_iam_policy.cc_callhome_policy[count.index].arn
-  role       = aws_iam_role.cc_node_iam_role[count.index].name
-}
-
-
-################################################################################
 # Define AWS Managed Secrets Manager Get Secrets Policy
 ################################################################################
 # Retrieve Secret Manager ARN by friendly name
