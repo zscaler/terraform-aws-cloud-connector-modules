21507cf65a35cfef53e405109d02b8b66e98ec2e
Jameson Molnar <JMolnar@zscaler.com>
2023-11-06 10:01:51 -0500

    refactor: add workload ZS Root CA install

--- modules/terraform-zscc-workload-aws/versions.tf
+++ modules/terraform-zscc-workload-aws/versions.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 4.59, <= 5.17"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.2.0"
+    }
   }
   required_version = ">= 0.13.7, < 2.0.0"
 }
