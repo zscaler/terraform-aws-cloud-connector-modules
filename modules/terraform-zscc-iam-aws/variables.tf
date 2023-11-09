bca6c655a0d64729a8fe279f08c79eacb2fcf0f6
Jameson Molnar <JMolnar@zscaler.com>
2023-11-09 10:45:36 -0500

    feat: add cc_tags iam policy

--- modules/terraform-zscc-iam-aws/variables.tf
+++ modules/terraform-zscc-iam-aws/variables.tf
@@ -44,3 +44,9 @@ variable "secret_name" {
   type        = string
   description = "AWS Secrets Manager Secret Name for Cloud Connector provisioning"
 }
+
+variable "cloud_tags_enabled" {
+  type        = bool
+  description = "Determines whether or not to create the cc_tags_policy IAM Policy and attach it to the CC IAM Role"
+  default     = false
+}
