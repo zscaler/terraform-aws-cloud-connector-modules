d1333dd7568a0b2f0f63f7856b58c21d1941e027
Venkatesh Krishnamurthy <vkrishnamurthy@zscaler.com>
2024-03-06 13:00:06 -0800

    Update python runtime to 3.12, upgraded aws provider verion to support python 3.12

--- modules/terraform-zscc-iam-aws/versions.tf
+++ modules/terraform-zscc-iam-aws/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.59, <= 5.17"
+      version = ">= 5.32.0, <= 5.39.1"
     }
   }
   required_version = ">= 0.13.7, < 2.0.0"
