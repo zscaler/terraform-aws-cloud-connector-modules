32859208235b8d61829951cb7b33c70d9b998dcc
Jameson Molnar <JMolnar@zscaler.com>
2024-04-09 10:59:43 -0400

    chore: cleanup old lambda artifacts

--- scripts/artifacts/creating_lambda_layer_payload.sh
+++
@@ -1,8 +0,0 @@
-mkdir mylambda-layer-with-requests290
-cd mylambda-layer-with-requests290
-python3 -m venv test_venv
-source test_venv/bin/activate
-python --version
-mkdir python
-pip install requests==2.29.0 -t python
-zip -r requests.zip python
--- scripts/zscaler_cc_lambda_service/version.py
+++
@@ -1,7 +0,0 @@
-"""
-Copyright (C) 2007-2023 Zscaler, Inc. All rights reserved.
-Unauthorized copying of this file, via any medium is strictly prohibited
-Proprietary and confidential
-"""
-# version.py
-VERSION = "1.0.5"
